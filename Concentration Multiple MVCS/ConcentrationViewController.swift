c71d48eaa40e7938daee02d6634e5a0d2c33ff1e
MrNoForks <testingiosapp12345@gmail.com>
2019-04-15 12:41:40 +0530

    cardButtons != nil i.e. to protect the code when MVC is getting prepared //segue always creates a brand new MVC // when we hit back the segued MVC get thrown away

--- Concentration Multiple MVCS/ConcentrationViewController.swift
+++ Concentration Multiple MVCS/ConcentrationViewController.swift
@@ -25,7 +25,7 @@ class ConcentrationViewController: UIViewController {
             updateFlipCountLabel()
         }
     }
-
+    
     
     private func updateFlipCountLabel(){
         //creating NSAttribute
@@ -47,7 +47,7 @@ class ConcentrationViewController: UIViewController {
             updateFlipCountLabel()
         }
     }
-   
+    
     
     @IBAction private func touchCard(_ sender: UIButton) {
         flipCount+=1
@@ -66,23 +66,26 @@ class ConcentrationViewController: UIViewController {
     
     private func updateViewFromModel(){
         //po cardButtons on console to see number coming nil  //left side thread error is called "call stack"
-        for index in cardButtons.indices
-        {
-            let button = cardButtons[index]
-            let card = game.cards[index]
-            if card.isFaceUp{
-                button.setTitle(emoji(for: card), for: .normal)
-                button.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+       // cardButtons != nil to protect against accessing cardButton when its not set i.e. in segue
+        if cardButtons != nil {
+            for index in cardButtons.indices
+            {
+                let button = cardButtons[index]
+                let card = game.cards[index]
+                if card.isFaceUp{
+                    button.setTitle(emoji(for: card), for: .normal)
+                    button.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+                }
+                else{
+                    button.setTitle("", for: .normal)
+                    button.backgroundColor = card.isMatched ?  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) :  #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+                }
+                
+                // MARK:- Crash App
+                //            if index > Int(arc4random_uniform(UInt32(100))){
+                //                exit(-1)
+                //            }
             }
-            else{
-                button.setTitle("", for: .normal)
-                button.backgroundColor = card.isMatched ?  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) :  #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
-            }
-            
-            // MARK:- Crash App
-//            if index > Int(arc4random_uniform(UInt32(100))){
-//                exit(-1)
-//            }
         }
     }
     
@@ -90,7 +93,7 @@ class ConcentrationViewController: UIViewController {
     var theme : String? {
         didSet{
             emojiChoices = theme ?? ""
-                //resetting emoji that we have set so far because it might be from different theme
+            //resetting emoji that we have set so far because it might be from different theme
             emoji = [:] // [:] empty dictionary because swift would be able to infer the type from  [Card:String]()
             //finally what if someone sets my view and i am in middle of the game ? I think i better update the View . So that whatever is showing  if it's got a wrong theme will update to new theme
             updateViewFromModel()
@@ -100,18 +103,18 @@ class ConcentrationViewController: UIViewController {
     private var emojiChoices = "👻🎃😈🍭😱🙀🍎🦇"
     
     //private var emoji = [Int:String]()
-
+    
     private var emoji = [Card:String]()
     
     private func emoji(for card : Card)-> String{
-
+        
         if emoji[card] == nil, emojiChoices.count>0{
-     
+            
             let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
-
-             emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
+            
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
-
+        
         return emoji[card] ?? "?"
     }
     
@@ -120,7 +123,7 @@ class ConcentrationViewController: UIViewController {
 extension Int{
     var arc4random : Int {
         if self > 0 {
-        return Int(arc4random_uniform(UInt32(self)))
+            return Int(arc4random_uniform(UInt32(self)))
         }
         else if self < 0{
             return -Int(arc4random_uniform(UInt32(abs(self))))
